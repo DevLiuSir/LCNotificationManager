61203691033e23214c578b9d5149facdedc27cc0
Marvin <93428739@qq.com>
2025-04-23 18:49:36 +0800

    Update LCNotificationCenterManager.swift

--- Sources/LCNotificationManager/Helpers/LCNotificationCenterManager.swift
+++ Sources/LCNotificationManager/Helpers/LCNotificationCenterManager.swift
@@ -13,18 +13,18 @@ import Foundation
 public class LCNotificationCenterManager {
     
     // MARK: - 发送通知
-    public static func postNotification(name: Notification.Name, userInfo: [AnyHashable: Any]? = nil) {
-        NotificationCenter.default.post(name: name, object: nil, userInfo: userInfo)
+    public static func postNotification(name: Notification.Name, object: Any? = nil, userInfo: [AnyHashable: Any]? = nil) {
+        NotificationCenter.default.post(name: name, object: object, userInfo: userInfo)
     }
     
     // MARK: - 添加通知观察者
-    public static func addNotification(name: Notification.Name, observer: Any, selector: Selector) {
-        NotificationCenter.default.addObserver(observer, selector: selector, name: name, object: nil)
+    public static func addNotification(name: Notification.Name, observer: Any, selector: Selector, object: Any? = nil) {
+        NotificationCenter.default.addObserver(observer, selector: selector, name: name, object: object)
     }
     
     // MARK: - 移除通知观察者
-    public static func removeNotification(name: Notification.Name, observer: Any) {
-        NotificationCenter.default.removeObserver(observer, name: name, object: nil)
+    public static func removeNotification(name: Notification.Name, observer: Any, object: Any? = nil) {
+        NotificationCenter.default.removeObserver(observer, name: name, object: object)
     }
     
     // MARK: - 移除所有通知
