9b499e95f877a35048c2842bec5bd363ab190ee3
Marvin <93428739@qq.com>
2026-09-04 00:40:21 +0800

    add demo

---
+++ Demo/LCNotificationManagerDemo/LCNotificationManagerDemo/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  LCNotificationManagerDemo
+//
+//
+//  Created by DevLiuSir on 2019/12/20.
+//
+    
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Demo/LCNotificationManagerDemo/LCNotificationManagerDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LCNotificationManagerDemo
+//
+//
+//  Created by DevLiuSir on 2019/12/20.
+//
+
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
